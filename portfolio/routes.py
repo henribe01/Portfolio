567bae7fb02a9d02752145a977c1507ef9a57c30
Cookieman01 <13785517+Cookieman01@users.noreply.github.com>
2022-06-30 21:00:40 +0200

    Created base.html, that every page uses

--- portfolio/routes.py
+++ portfolio/routes.py
@@ -1,7 +1,24 @@
-from portfolio import app
 from flask import render_template
+
+from portfolio import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects-grid-cards.html')
+
+
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
