89e7b047457c6fd6de74176e581bb1fc0bd8c9e8
Henri Benecke <bba3815@studium.uni-hamburg.de>
2026-03-15 10:58:03 +0100

    Revert "Fix PostgreSQL URI format and add Render configuration"
    This reverts commit 43fbbca5c57009f3ec9e523d04801f67ba9b4ed8.

--- config.py
+++ config.py
@@ -9,11 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'test'
 
     # SQLAlchemy configuration
-    database_url = os.environ.get('DATABASE_URL')
-    if database_url and database_url.startswith('postgres://'):
-        database_url = database_url.replace('postgres://', 'postgresql://', 1)
-
-    SQLALCHEMY_DATABASE_URI = database_url or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(
         os.path.dirname(__file__), 'portfolio.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
